f63b4a2661201ff6b51cd55ab9d014667abc03a7
KK Surendran <137263515+KKSurendran06@users.noreply.github.com>
2024-01-27 14:04:33 +0530

    Update models.py

--- flutter_app/models.py
+++ flutter_app/models.py
@@ -2,13 +2,15 @@ from django.contrib.auth.models import User
 from django.db import models
 
 class EventPage(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE,default = 1)
+    username =  models.CharField(max_length=20, default = 'gamma')
+    colindex = models.IntegerField(default = 2)
     heading = models.CharField(max_length=20)
     description = models.CharField(max_length=250)
-    location = models.CharField(max_length = 10, default='Delhi')
-    event_time = models.CharField(max_length=10)
-    pass_out_year = models.CharField(max_length=4)
-    event_date = models.CharField(max_length=10)
+    location = models.CharField(max_length = 25, default='Delhi')
+    event_time = models.CharField(max_length=20)
+    pass_out_year = models.CharField(max_length=20,default = "2020")
+    event_date = models.CharField(max_length=20,default='12/12/2012')
+    department = models.CharField(max_length=20,default='CSE')
 class RaiseHand(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE,default = 1)
     raisehand = models.BooleanField()
@@ -20,7 +22,7 @@ class RequestInstitute(models.Model):
 class InstituteDetails(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE,default = 1)
     department = models.CharField(max_length=20)
-    graduation_year = models.CharField(max_length=10)
+    graduation_year = models.DateField()
 
 class EditProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE,default = 1)
